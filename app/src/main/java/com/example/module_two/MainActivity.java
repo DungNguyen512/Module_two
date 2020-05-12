56cafcf1d2e313a2a5076f60a609656ff5ecbcf5
DungNguyen512 <dungipn512@gmail.com>
2020-05-12 20:26:58 +0700

    1 - Module Two 11/05/2020

--- app/src/main/java/com/example/module_two/MainActivity.java
+++ app/src/main/java/com/example/module_two/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.module_two;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 public class MainActivity extends AppCompatActivity {
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.my_view_pager);
     }
 }
